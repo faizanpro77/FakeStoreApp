874ba0f0efdda7495c31b908c523821e472ca969
ShaikhFaizaniOS <faizanshikh7798@gmail.com>
2025-04-12 18:24:29 +0530

    Initial Commit

---
+++ FakeStoreApp/FakeStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreAppApp.swift
+//  FakeStoreApp
+//
+//  Created by shaikh faizan on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeStoreAppTests/FakeStoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreAppTests.swift
+//  FakeStoreAppTests
+//
+//  Created by shaikh faizan on 12/04/25.
+//
+
+import Testing
+@testable import FakeStoreApp
+
+struct FakeStoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
